c19d252f42cc3693cef02c75fa1487c2eff9abcf
Jacek Lach <jacek@lshift.net>
2013-08-20 17:34:46 +0100

    Add license, release as 0.01

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject org.clojars.jaceklach/midje-junit-formatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.jaceklach/midje-junit-formatter "0.0.1"
+  :description "Fork of midje-junit-formatter with clojure.test compatibility"
+  :url "https://github.com/JacekLach/midje-junit-formatter"
+  :scm {:name "git"
+        :dir  "."
+        :url "https://github.com/JacekLach/midje-junit-formatter"
+        :tag  "HEAD"}
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.1"]
                  [midje "1.5.1"]
